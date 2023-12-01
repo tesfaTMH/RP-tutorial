07647e0b2b5c409d2a0ebde4374212ae25ea53dc
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2023-12-01 12:57:27 +0200

    django-blog-app with added some dummy posts, starter bootstrap templates and some more styles

--- blog-app-with-django/django_blog_app/blog/views.py
+++ blog-app-with-django/django_blog_app/blog/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+# dummy data for the posts
+posts = [
+    {
+        'author': 'Abebe',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 12, 2023'
+    },
+    {
+        'author': 'Tesfu',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 11, 2023'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Welcome to Blog app home page.</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context=context)
 
 def about(request):
-    return HttpResponse('<h1>Welcome to Blog app about page</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
