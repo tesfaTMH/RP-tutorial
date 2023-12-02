e0264aec74fbc4a6ce86dcbb177455ad89bf2246
Tesfamichael Hagos <hagos.tesfam@gmail.com>
2023-12-02 08:32:28 +0200

    models for user, posts created and superuser added

--- blog-app-with-django/django_blog_app/blog/views.py
+++ blog-app-with-django/django_blog_app/blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
-# dummy data for the posts
-posts = [
-    {
-        'author': 'Abebe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 12, 2023'
-    },
-    {
-        'author': 'Tesfu',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 11, 2023'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context=context)
 
